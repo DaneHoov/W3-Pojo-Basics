a31ccd46001052b0117d34a67ce39b815829d7e4
danehoov <DaneHoov22@gmail.com>
2024-03-30 09:53:00 -0600

    problem 11 solved

--- problems/11-count-scores.js
+++ problems/11-count-scores.js
@@ -2,8 +2,8 @@
 Write a function `countScores(people)` that takes in an array of score
 objects (people) as its input. A score object has two key-value pairs:
 the scorer (string) and a point value (number). `countScores(people)` should
-return an object that has key-value pairs each person who scored as a key
-and the sum of the total points for the game as  listingtheir value.
+return an object that has key-value pairs listing each person who scored as a key
+and the sum of the total points for the game as their value.
 
 
 Example 1:
@@ -28,17 +28,27 @@ console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 ***********************************************************************/
 
 function countScores(people) {
+  let totalScore = {};
 
-  let scores = people.reduce((score))
-
+  for (let person of people) {
+    let name = person.name;
+    let score = person.score;
+    if (totalScore[name] === undefined) {
+      totalScore[name] = score;
+    }
+    else {
+      totalScore[name] += score;
+    }
+  }
+  return totalScore;
 }
+
 let ppl = [{ name: "Anthony", score: 10 },
 { name: "Fred", score: 10 },
 { name: "Anthony", score: -8 },
 { name: "Winnie", score: 12 }];
 
 console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
-
 
 let peeps = [
   { name: "Anthony", score: 2 },
